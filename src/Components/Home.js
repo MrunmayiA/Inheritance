3d28a18a609e8dddb10b7f858dc4582a458d5cef
RiddhiDapke <rbdapke_b22@it.vjti.ac.in>
2024-01-27 22:38:18 +0530

    HopePageAdded

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "./Navbar";
+const Home = () => {
+  return (
+    <div classname="home-container">
+      <Navbar/>
+      <div classname="home-banner-container"></div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Home
